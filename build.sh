45e65f80d1e63e7a50e89024074963eb7616e4aa
Alice Rodrigues <alicerodrig1@gmail.com>
2024-03-16 17:01:29 -0300

    Updated build script

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+mkdir -p bin
 nasm src/bootsector.asm -f bin -o bin/bootsector.bin
 nasm src/kernel.asm -f bin -o bin/kernel.bin
 cd bin/
 cat bootsector.bin kernel.bin > simplicity.bin
 qemu-system-i386 -fda simplicity.bin
+cd ..
+rm -rf bin
